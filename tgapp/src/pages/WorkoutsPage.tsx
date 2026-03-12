9202c1f185f3702d4cf1c9d7e79d1101aefe5640
BelozerovaA <belozerovaa256@gmail.com>
2026-03-12 16:53:11 +0300

    test

--- tgapp/src/pages/WorkoutsPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function WorkoutsPage() {
-  return (
-    <div className="flex items-center justify-center h-[60vh]">
-      <p className="text-xl font-bold opacity-40">💪 Тренировки</p>
-    </div>
-  );
-}
